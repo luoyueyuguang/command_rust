c379e5911c82ed094f88ee9a0ae073460def7a14
luoyueyuguang <luoyuelight@outlook.com>
2024-05-21 16:11:30 +0800

    clean some files

--- minigrep/target/debug/deps/minigrep-46c773144ad78de0.d
+++
@@ -1,5 +0,0 @@
-/home/luoyue/projects/rust_test/command_rust/minigrep/target/debug/deps/minigrep-46c773144ad78de0: src/main.rs
-
-/home/luoyue/projects/rust_test/command_rust/minigrep/target/debug/deps/minigrep-46c773144ad78de0.d: src/main.rs
-
-src/main.rs:
--- minigrep/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/home/luoyue/projects/rust_test/command_rust/minigrep/target/debug/minigrep: /home/luoyue/projects/rust_test/command_rust/minigrep/src/main.rs
